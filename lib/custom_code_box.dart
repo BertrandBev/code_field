65b79bb0e56573a951cebf35e30ed5eefb3dcfa2
svetlana.ryzhkova <svetlana.ryzhkova@akvelon.com>
2021-12-14 16:41:26 +0300

    add buttons language, theme and reset; add scrollView

---
+++ lib/custom_code_box.dart
@@ -0,0 +1,158 @@
+import 'themes.dart';
+import 'package:flutter/material.dart';
+import 'code_text_field.dart';
+import 'package:highlight/languages/all.dart';
+
+class CustomCodeBox extends StatefulWidget {
+  final String language;
+  final String theme;
+
+  const CustomCodeBox({Key? key, required this.language, required this.theme})
+      : super(key: key);
+
+  @override
+  _CustomCodeBoxState createState() => _CustomCodeBoxState();
+}
+
+class _CustomCodeBoxState extends State<CustomCodeBox> {
+  String? language;
+  String? theme;
+  bool? reset;
+
+  @override
+  void initState() {
+    super.initState();
+    language = widget.language;
+    theme = widget.theme;
+    reset = false;
+  }
+
+  List<String?> languageList = <String>[
+    "java",
+    "go",
+    "python",
+    "scala",
+    "dart"
+  ];
+
+  List<String?> themeList  = <String>[
+    "monokai-sublime",
+    "a11y-dark",
+    "an-old-hope",
+    "vs2015",
+    "vs",
+    "atom-one-dark"
+  ];
+
+  Widget buildDropdown(Iterable<String?> choices, String value, IconData icon,
+      Function(String?) onChanged) {
+    return DropdownButton<String>(
+      value: value,
+      items: choices.map((String? value) {
+        return DropdownMenuItem<String>(
+          value: value,
+          child: value == null
+              ? const Divider()
+              : Text(value, style: const TextStyle(color: Colors.white)),
+        );
+      }).toList(),
+      icon: Icon(icon, color: Colors.white),
+      onChanged: onChanged,
+      dropdownColor: Colors.black87,
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final codeDropdown =
+        buildDropdown(languageList, language!, Icons.code, (val) {
+      if (val == null) return;
+      setState(() => language = val);
+    });
+    final themeDropdown =
+        buildDropdown(themeList, theme!, Icons.color_lens, (val) {
+      if (val == null) return;
+      setState(() => theme = val);
+    });
+    final resetButton = TextButton.icon(
+      icon: const Icon(Icons.delete, color: Colors.white), 
+      label: const Text('Reset', style: TextStyle(color: Colors.white)),
+      onPressed: () {
+        setState(() {
+          reset = (!reset!);
+        });
+      }, 
+    );
+
+    final buttons = Container (
+      height: MediaQuery.of(context).size.height/13,
+      color: Colors.deepPurple[900],
+      child: Row(
+        children: [
+        SizedBox(width: 25.0),
+        Text('Code editor', style: TextStyle(fontSize: 28, color: Colors.white),),
+        SizedBox(width: 510.0),
+        codeDropdown,
+        SizedBox(width: 12.0),
+        themeDropdown,
+        SizedBox(width: 12.0),
+        resetButton
+        ]
+      )
+    );
+    final codeField = InnerField(
+      key: ValueKey("$language - $theme - $reset"),
+      language: language!,
+      theme: theme!,
+    );
+    return Column(children: [
+      buttons,
+      codeField,
+    ]);
+  }
+}
+
+class InnerField extends StatefulWidget {
+  final String language;
+  final String theme;
+
+  const InnerField({Key? key, required this.language, required this.theme})
+      : super(key: key);
+
+  @override
+  _InnerFieldState createState() => _InnerFieldState();
+}
+
+class _InnerFieldState extends State<InnerField> {
+  CodeController? _codeController;
+
+  @override
+  void initState() {
+    super.initState();
+    _codeController = CodeController(
+      language: allLanguages[widget.language],
+      theme: THEMES[widget.theme],
+    );
+  }
+
+  @override
+  void dispose() {
+    _codeController?.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    
+    return Container(
+      color: _codeController!.theme!['root']!.backgroundColor,
+      height: MediaQuery.of(context).size.height / 13 * 12,
+      child: SingleChildScrollView(
+        child: CodeField(
+          controller: _codeController!,
+          textStyle: const TextStyle(fontFamily: 'SourceCode'),
+        )
+      ),
+    );
+  }
+}
